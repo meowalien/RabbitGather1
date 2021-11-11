89070298f2ece4b12a9f43312da91614d984f7a0
Jacky <jacky.li@joy-games.online>
2021-11-11 18:39:44 +0800

    fix:remove .idea folder

--- view/official_page/src/view/Home.vue
+++ view/official_page/src/view/Home.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="h-[100vh] w-[100vw] bg-blue-300">
+  <div class="h-[100vh] w-[100vw] bg-white flex flex-col justify-center items-center">
 
+    <div class="bg-white h-96 w-72 rounded-4xl shadow-2rg p-7 flex flex-col justify-between">
+      <div>
+        <img src="/favicon.ico" alt="" class="block">
+      </div>
+      <div class="w-full flex flex-row items-baseline">
+        <p class="min-w-max mr-2"> 帳號：</p><input type="text" class="py-3 px-4 bg-white rounded-lg placeholder-gray-400 text-gray-900 appearance-none inline-block w-full shadow-md focus:outline-none focus:ring-2 focus:ring-pink-300">
+      </div>
+      <div class="w-full flex flex-row items-baseline">
+        <p class="min-w-max mr-2"> 帳號：</p><input type="text" class="py-3 px-4 bg-white rounded-lg placeholder-gray-400 text-gray-900 appearance-none inline-block w-full shadow-md focus:outline-none focus:ring-2 focus:ring-pink-300">
+      </div>
+    </div>
   </div>
 
 </template>
--- view/official_page/tailwind.config.js
+++ view/official_page/tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
   purge: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      borderRadius:{
+        '4xl':'1.75rem'
+
+      },
+      boxShadow:{
+        'rg': '0 0px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+        '1rg': '0 0px 12px -1px rgba(0, 0, 0, 0.2), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+        '2rg': '0 0px 16px -1px rgba(0, 0, 0, 0.3), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+      }
+    },
   },
   plugins: [],
 }
